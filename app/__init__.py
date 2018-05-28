b3cb4cbdbf793dafc9c32c33cb168c6be2b3ad70
sameer <sameerbasha170794@gmail.com>
2018-05-28 17:02:16 +0530

    b

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
-from flask_oauth import OAuth
+
 
 
 app = Flask(__name__)
